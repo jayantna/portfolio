bc82af0703c7ee0046dc4ab4d14aa304803b161c
jayantna <jonty.nagle@gmail.com>
2024-07-17 18:50:37 +0530

    Added Contact page

---
+++ app/contact/page.tsx
@@ -0,0 +1,17 @@
+import { genPageMetadata } from 'app/seo'
+
+export const metadata = genPageMetadata({ title: 'Contact' })
+export default function Page() {
+  return (
+    <>
+      <div className="divide-y divide-gray-200 dark:divide-gray-700">
+        <div className="space-y-2 pb-6 pt-6 md:space-y-5">
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-4xl md:leading-14">
+            Contact
+          </h1>
+        </div>
+        <div className="container py-12"></div>
+      </div>
+    </>
+  )
+}
