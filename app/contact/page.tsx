4db7d7618b4b424f82990ace3e2f1eb65696de50
jayantna <jonty.nagle@gmail.com>
2024-07-20 15:47:17 +0530

    Remove tag page and add contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
     <>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pb-6 pt-6 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-4xl md:leading-14">
+          <h1 className="text-3xl font-extrabold leading-9 text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-4xl md:leading-14">
             Contact
           </h1>
         </div>
