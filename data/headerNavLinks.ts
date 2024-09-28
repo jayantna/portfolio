d9f9d7ddc442be0aed38d2d4ba827215cc638658
jayantna <jonty.nagle@gmail.com>
2024-09-28 09:50:20 +0530

    Hide blog and projects

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,9 +1,9 @@
 const headerNavLinks = [
-  { href: '/', title: 'Home' },
-  { href: '/blog', title: 'Blog' },
-  { href: '/projects', title: 'Projects' },
+  // { href: '/blog', title: 'Blog' },
+  // { href: '/projects', title: 'Projects' },
+  { href: '/experience', title: 'Experience' },
   { href: '/about', title: 'About' },
   { href: '/contact', title: 'Contact' },
-]
+];
 
-export default headerNavLinks
+export default headerNavLinks;
