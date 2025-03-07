6298c8e1a21e578f202d5c45a60597448a3d9237
jayantna <jonty.nagle@gmail.com>
2025-03-07 18:33:42 +0530

    Change swipper location

--- app/Main.tsx
+++ app/Main.tsx
@@ -78,45 +78,7 @@ export default function Home() {
         <span className="hover:text-primary-500">I ship. </span>
       </div>
 
-      <div className="pointer-events-none relative my-8 hidden lg:block">
-        <div className="absolute left-0 top-0 z-10 h-full w-[20%] bg-gradient-to-r from-white to-transparent dark:from-gray-950"></div>
-        {/* Right overlay with gradient from right to transparent */}
-        <div className="absolute right-0 top-0 z-10 h-full w-[20%] bg-gradient-to-l from-white to-transparent dark:from-gray-950"></div>
-        {/* Left overlay with gradient from left to transparent */}
-        <Swiper
-          className="swiper-container"
-          loop={true}
-          freeMode={true}
-          spaceBetween={30}
-          grabCursor={false}
-          slidesPerView={9}
-          autoplay={{
-            delay: 0,
-            disableOnInteraction: false,
-          }}
-          speed={4000}
-          modules={[Autoplay]}
-        >
-          {Array.from({ length: 18 }, (_, index) => (
-            <SwiperSlide key={index}>
-              <div key={index} className="logo-container p-4">
-                <Image
-                  src={`/static/images/logo/${index}.png`}
-                  alt={index.toString()}
-                  width={80}
-                  height={80}
-                  key={index}
-                />
-              </div>
-              <div className="rounded-b-lg border-b-4 border-gray-50 p-2 text-center font-sourceCode text-sm text-gray-700 dark:border-gray-900 dark:text-gray-600">
-                {logoList[index]}
-              </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </div>
-
-      <div className="flex flex-col items-center justify-between py-12 sm:flex-row">
+      <div className="flex flex-col items-center justify-between py-8 sm:flex-row">
         <a
           href="/contact"
           className="m-4 flex w-full cursor-pointer flex-col justify-between rounded-lg border border-gray-300 bg-white p-4 shadow-md transition-shadow duration-300 hover:shadow-lg dark:border-gray-600 dark:bg-gray-900 sm:w-1/3 md:min-h-[130px] lg:min-h-[130px]"
@@ -160,6 +122,44 @@ export default function Home() {
           </div>
           <p className="mt-2 text-gray-500 dark:text-gray-400">Get a copy of my resume.</p>
         </a>
+      </div>
+
+      <div className="relative my-4 hidden lg:block">
+        <div className="absolute left-0 top-0 z-10 h-full w-[20%] bg-gradient-to-r from-white to-transparent dark:from-gray-950"></div>
+        {/* Right overlay with gradient from right to transparent */}
+        <div className="absolute right-0 top-0 z-10 h-full w-[20%] bg-gradient-to-l from-white to-transparent dark:from-gray-950"></div>
+        {/* Left overlay with gradient from left to transparent */}
+        <Swiper
+          className="swiper-container"
+          loop={true}
+          freeMode={true}
+          spaceBetween={30}
+          grabCursor={true}
+          slidesPerView={9}
+          autoplay={{
+            delay: 0,
+            disableOnInteraction: false,
+          }}
+          speed={4000}
+          modules={[Autoplay]}
+        >
+          {Array.from({ length: 18 }, (_, index) => (
+            <SwiperSlide key={index}>
+              <div key={index} className="logo-container p-4">
+                <Image
+                  src={`/static/images/logo/${index}.png`}
+                  alt={index.toString()}
+                  width={80}
+                  height={80}
+                  key={index}
+                />
+              </div>
+              <div className="p-2 text-center font-sourceCode text-sm text-gray-700 dark:border-gray-900 dark:text-gray-600">
+                {logoList[index]}
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </>
   );
